c66812c710a373f364231be37e359e68e9ee53e1
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-10-29 10:34:39 +0530

    setup completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>} >
+          </Route>
+          <Route path='*' element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
