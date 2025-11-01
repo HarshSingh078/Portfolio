b9a2d16ddd084fbfe62d8593f39d2babef332404
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-01 19:45:15 +0530

    Toggle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-export const Home = () => {
-    return <div className="min-h-screen">
+import { ThemeToggle } from "../components/ThemeToggle"
 
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
+        <ThemeToggle/>
     </div>
 }
