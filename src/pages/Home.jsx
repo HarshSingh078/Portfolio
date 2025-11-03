dbef635748514809230ce566f4c819922f110c33
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-03 11:57:24 +0530

    about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "../components/StarBackground"
 import { Navbar } from "../components/Navbar"
 import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
         <ThemeToggle/>
@@ -9,6 +10,7 @@ export const Home = () => {
         <Navbar/>
         <main>
             <HeroSection/>
+            <AboutSection/>
         </main>
     </div>
 }
