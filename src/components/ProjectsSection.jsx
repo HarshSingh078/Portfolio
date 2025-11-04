262b234da53da328e995599d9a1e4e02ea4c1f8d
Harsh Singh <harshsinghkumwar97819@gmail.com>
2025-11-04 10:19:51 +0530

    Projects added

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,5 +1,34 @@
 
+const projects = [
+    {
+        id : 1 ,
+        title : "Jokes Generator" ,
+        description : "A simple, fun web application that generates random jokes with the click of a button." ,
+        image : "/projects/project1.png" ,
+        tags : ["HTML" , "CSS" , "JS" , "API"] ,
+        demoUrl : "https://jokes-generator-five-umber.vercel.app/" ,
+        githubUrl : "https://github.com/HarshSingh078/Jokes-Generator"
+    } , 
+    {
+        id : 2 ,
+        title : "BMI Calculator" ,
+        description : "A simple and intuitive web application to calculate a user's Body Mass Index (BMI) using their height and weight." ,
+        image : "/projects/project2.png" ,
+        tags : ["HTML" , "CSS" , "JS" , "API"] ,
+        demoUrl : " https://bmi-calculator-app-red.vercel.app/" ,
+        githubUrl : "https://github.com/HarshSingh078/BMI-Calculator-App"
+    } , 
+    {
+        id : 3 ,
+        title : "Lyrics Finder" ,
+        description : "This is a simple web application that allows users to search for song lyrics. Built with HTML, CSS, JavaScript and ReactJS, it uses a public API to fetch and display the lyrics for any song." ,
+        image : "/projects/project1.png" ,
+        tags : ["HTML" , "CSS" , "JS" , "ReactJS" , "API"] ,
+        demoUrl : " https://lyrics-finder-iota.vercel.app/" ,
+        githubUrl : "https://github.com/HarshSingh078/Lyrics-Finder"
+    } , 
+] 
 
 export const ProjectsSection = () => {
-    return <div></div>
+    return <section></section>
 }
